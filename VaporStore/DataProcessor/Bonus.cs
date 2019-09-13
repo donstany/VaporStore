49827ddf14c30dd59e9c526a153586f2e05ab2bb
donstany <donstany79@gmail.com>
2019-09-13 16:37:51 +0300

    Initial commit

---
+++ VaporStore/DataProcessor/Bonus.cs
@@ -0,0 +1,15 @@
+﻿namespace VaporStore.DataProcessor
+{
+	using System;
+	using Data;
+
+	public static class Bonus
+	{
+		public static string UpdateEmail(VaporStoreDbContext context, string username, string newEmail)
+		{
+            //throw new NotImplementedException();
+            Console.ReadKey();
+            return string.Empty;
+		}
+	}
+}
